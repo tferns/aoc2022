b500e68f136e49d32389e9ddc2edcd63ae623c2b
tferns <thomas.ferns@version1.com>
2022-12-01 17:41:18 +0000

    Initial commit

---
+++ day01/part2.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    elves = s.split("\n\n")
+    elves_totals = [sum([int(y) for y in x.split("\n") if y]) for x in elves]
+    top3 = sorted(elves_totals)[-3:]
+    return sum(top3)
+
+
+INPUT_S = '''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+EXPECTED = sum([24000, 11000, 10000])
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
