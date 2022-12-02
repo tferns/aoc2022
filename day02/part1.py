a57fb6b4f57213e96bbf7a2b19a5000da1d3c225
tferns <thomas.ferns@version1.com>
2022-12-02 17:38:04 +0000

    Something something elves rock paper scissors

---
+++ day02/part1.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+SCORE_MAP = {'X': 1, 'Y': 2, 'Z': 3, }
+
+WIN_MAP = {'X': 'C', 'Y': 'A', 'Z': 'B'}
+DRAW_MAP = {'X': 'A', 'Y': 'B', 'Z': 'C'}
+LOSE_MAP = {'X': 'B', 'Y': 'C', 'Z': 'A'}
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+
+    score = 0
+    for line in lines:
+        theirs, ours = line.split()
+
+        if WIN_MAP[ours] == theirs:
+            score += 6
+        elif DRAW_MAP[ours] == theirs:
+            score += 3
+
+        score += SCORE_MAP[ours]
+
+    return score
+
+
+INPUT_S = '''\
+A Y
+B X
+C Z
+'''
+EXPECTED = 15
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
