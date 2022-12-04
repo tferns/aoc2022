23776522c5ad99ac0bf7faae1345940489103d2e
tferns <thomas.ferns@version1.com>
2022-12-04 05:19:05 +0000

    Overlapping elves

---
+++ day04/part1.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    score = 0
+
+    lines = s.splitlines()
+    for line in lines:
+        a, b = line.split(",")
+        a1, a2, b1, b2 = a.split("-") + b.split("-")
+
+        if int(a1) <= int(b1) and int(a2) >= int(b2):
+            score += 1
+        elif int(a1) >= int(b1) and int(a2) <= int(b2):
+            score += 1
+
+    return score
+
+
+INPUT_S = '''\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+'''
+EXPECTED = 2
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ day04/part2.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    score = 0
+
+    lines = s.splitlines()
+    for line in lines:
+        a, b = line.split(",")
+        a1, a2, b1, b2 = a.split("-") + b.split("-")
+
+        range_a = range(int(a1), int(a2) + 1)
+        range_b = range(int(b1), int(b2) + 1)
+        if set(range_a) & set(range_b):
+            score += 1
+
+    return score
+
+
+INPUT_S = '''\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+'''
+EXPECTED = 4
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
