66e7d2b29bb0a025e876d79679dd5b019d528858
tferns <thomas.ferns@version1.com>
2022-12-18 13:46:30 +0000

    Add day 18 part 2

--- day18/part2.py
+++ day18/part2.py
@@ -11,12 +11,75 @@ INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 def compute(s: str) -> int:
-    lines = s.splitlines()
     positions = []
-    for line in lines:
-        x,y,z = line.split(',')
-        x,y,z = int(x), int(y), int(z)
+    for line in s.splitlines():
+        x, y, z = line.split(',')
+        x, y, z = int(x), int(y), int(z)
         positions.append((x, y, z))
+
+    min_x = min([x for x, *_ in positions])
+    max_x = max([x for x, *_ in positions])
+    min_y = min([y for _, y, _ in positions])
+    max_y = max([y for _, y, _ in positions])
+    min_z = min([z for *_, z in positions])
+    max_z = max([z for *_, z in positions])
+
+    spaces = []
+    for x in range(min_x, max_x + 1):
+        for y in range(min_y, max_y + 1):
+            for z in range(min_z, max_z + 1):
+                if (x, y, z) not in positions:
+                    spaces.append((x, y, z))
+
+    space_is_outside_map = {}
+    for x, y, z in spaces:
+        if any([
+            x == max_x, x == min_x,
+            y == max_y, y == min_y,
+            z == max_z, z == min_z
+        ]):
+            space_is_outside_map[(x, y, z)] = True
+        else:
+            space_is_outside_map[(x, y, z)] = False
+
+    for dist in range(1, (max(max_x, max_y, max_z) // 2) + 1):
+        for x, y, z in spaces:
+            space_is_outside = space_is_outside_map[(x, y, z)]
+            if space_is_outside:
+                continue
+
+            if any([
+                x == max_x - dist, x == min_x + dist,
+                y == max_y - dist, y == min_y + dist,
+                z == max_z - dist, z == min_z + dist
+            ]):
+                if (x + 1, y, z) in spaces:
+                    adj_space_is_outside = space_is_outside_map[(x + 1, y, z)]
+                    if adj_space_is_outside:
+                        space_is_outside_map[(x, y, z)] = True
+                if (x - 1, y, z) in spaces:
+                    adj_space_is_outside = space_is_outside_map[(x - 1, y, z)]
+                    if adj_space_is_outside:
+                        space_is_outside_map[(x, y, z)] = True
+                if (x, y + 1, z) in spaces:
+                    adj_space_is_outside = space_is_outside_map[(x, y + 1, z)]
+                    if adj_space_is_outside:
+                        space_is_outside_map[(x, y, z)] = True
+                if (x, y - 1, z) in spaces:
+                    adj_space_is_outside = space_is_outside_map[(x, y - 1, z)]
+                    if adj_space_is_outside:
+                        space_is_outside_map[(x, y, z)] = True
+                if (x, y, z + 1) in spaces:
+                    adj_space_is_outside = space_is_outside_map[(x, y, z + 1)]
+                    if adj_space_is_outside:
+                        space_is_outside_map[(x, y, z)] = True
+                if (x, y, z - 1) in spaces:
+                    adj_space_is_outside = space_is_outside_map[(x, y, z - 1)]
+                    if adj_space_is_outside:
+                        space_is_outside_map[(x, y, z)] = True
+
+    positions.extend(s for s in spaces if not space_is_outside_map[s])
+
     surface = 0
     for x, y, z in positions:
         sides = 6
@@ -33,6 +96,7 @@ def compute(s: str) -> int:
         if (x, y, z - 1) in positions:
             sides -= 1
         surface += sides
+
     return surface
 
 
@@ -51,13 +115,13 @@ INPUT_S = '''\
 2,1,5
 2,3,5
 '''
-EXPECTED = 64
+EXPECTED = 58
 
 
 @pytest.mark.parametrize(
     ('input_s', 'expected'),
     (
-        (INPUT_S, EXPECTED),
+            (INPUT_S, EXPECTED),
     ),
 )
 def test(input_s: str, expected: int) -> None:
